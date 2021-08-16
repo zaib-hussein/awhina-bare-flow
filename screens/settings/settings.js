a1bd2ef81f7589def2833681e91692a3535a9af0
cf-whyte <cfwhytedev@gmail.com>
2021-08-16 21:23:27 +1200

    Refactoring project structure, screens has folders per screen, eslint and prettier configs included

---
+++ screens/settings/settings.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {StyleSheet, Text, View, TextInput, Button} from 'react-native';
+
+export default function Settings() {
+  return (
+    <View style={styles.container}>
+      <View></View>
+      <Text> Setting </Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'tomato',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
