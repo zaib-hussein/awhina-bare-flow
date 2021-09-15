4df4e8fb747a7476093d5a45f2a9a53c80f360ec
pcb1469 <pcb1469@autuni.ac.nz>
2021-09-15 12:57:53 +1200

    map/show location

--- screens/map.js
+++ screens/map.js
@@ -1,13 +1,49 @@
+import { StyleSheet, Text,Platform,View} from 'react-native';
+import MapView, {PROVIDER_GOOGLE, Marker} from 'react-native-maps';
+import React, {Component} from 'react';
 
-import React from 'react';  
-import { StyleSheet, Text, View, TextInput, Button} from 'react-native';
+class Map extends Component {
 
-export default function Map() {
-    return ( 
-        <View>
-            <Text> Map </Text>
-        </View>
-    )
-}
+    render() {
+      return (
+          <MapView
+              showsUserLocation={true}
+              style={styles.map}
+              initialRegion={{
+                latitude: -36.858755,
+                longitude: 174.76163,
+                latitudeDelta: 0.0812,
+                longitudeDelta: 0.0402,
+                
+              }}
+          />
+      );
+    }
+  
+  }
+
+  export default Map;
+
+  const styles = StyleSheet.create({
+  container: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  map: {
+    // ...StyleSheet.absoluteFillObject
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    //   width: Dimensions.get('window').width,
+    //   height: 500,
+  },
+});
 
 ///
