56666a8e38b972a2b3ec7b81629509cbaea1c814
cf-whyte <cfwhytedev@gmail.com>
2021-10-06 11:59:47 +1300

    Emergency-Option component

---
+++ screens/emergency-option.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import {TouchableOpacity, Alert, StyleSheet, Image, Text} from 'react-native';
+
+export class EmergencyOption extends React.Component {
+  getUri() {
+    switch (this.props.type) {
+      case 'water':
+        return require('./icons/water.png');
+        break;
+      case 'clothes':
+        return require('./icons/clothes.png');
+        break;
+      case 'food':
+        return require('./icons/food.png');
+        break;
+      case 'lost':
+        return require('./icons/lost.png');
+        break;
+      case 'shelter':
+        return require('./icons/shelter.png');
+        break;
+      default:
+        return require('./icons/question-mark.png')
+        break;
+    }
+  }
+
+  getIsHelp() {
+    return this.props.isHelp ? 'need' : 'can give';
+  }
+
+  getIsHelpTitle(){
+    return this.props.isHelp ? 'We will alert the community' : 'We will let the community know';
+  }
+
+  render() {
+    return (
+      <>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => {
+            Alert.alert(`${this.getIsHelpTitle()}`, `You ${this.getIsHelp()} ${this.props.type}`);
+          }}
+        >
+          <Image source={this.getUri()} />
+        </TouchableOpacity>
+        <Text style={styles.optionTitle}>{this.props.type}</Text>
+      </>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  button: {
+    backgroundColor: '#FFF',
+    borderRadius: 10,
+    shadowColor: '#ff0000',
+    shadowOffset: {width: 1, height: 5},
+    shadowRadius: 10,
+    shadowOpacity: 0.35,
+  },
+  optionTitle: {
+    textTransform: 'uppercase',
+  },
+});
