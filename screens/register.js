a4f8efe56f4defd8d9486f2fcafe04782d5f61d2
cf-whyte <77770364+cf-whyte@users.noreply.github.com>
2021-10-15 21:58:35 +1300

    Error messages showing nicely

--- screens/register.js
+++ screens/register.js
@@ -8,6 +8,7 @@ import {
 	StyleSheet,
 	StatusBar,
 	Keyboard,
+	KeyboardAvoidingView,
 	Dimensions,
 	Button,
 	ToastAndroid,
@@ -23,44 +24,29 @@ import validator from 'validator';
 export default function Register() {
 	const [firstName, setFirstName] = useState('');
 	const [lastName, setLastName] = useState('');
+	const [tempPassword, setTempPassword] = useState('');
 	const [password, setPassword] = useState('');
 	const [email, setEmail] = useState('');
 	const [phone, setPhone] = useState('+64');
 	const [DOB, setDOB] = useState('');
 	const [isFirstName, setIsFirstName] = useState(true);
 	const [isLastName, setIsLastName] = useState(true);
-	const [isPassword, setIsPassword] = useState(true);
 	const [isEmail, setIsEmail] = useState(true);
-	const [isPhone, setIsPhone] = useState(true);
 	const [isDOB, setIsDOB] = useState(true);
+	const [isPhone, setIsPhone] = useState(true);
+	const [isPassword, setIsPassword] = useState(true);
 	const [hideDOBPicker, setHideDOBPicker] = useState(true);
 	const [hidePassword, setHidePassword] = useState(true);
+	const [hideTempPassword, setHideTempPassword] = useState(true);
 
 	//Uses the validator to validate user inputs
 	function validateInput() {
-		if (
-			validator.isEmail(email) &&
-			password.length >= 6 &&
-			validator.isMobilePhone(phone) &&
-			validator.isAlpha(firstName) &&
-			validator.isAlpha(lastName)
-		) {
-			return true;
-		} else if (!isEmail) {
-			ToastAndroid.show('Must use a valid email.', ToastAndroid.LONG);
-			return false;
-		} else if (!isFirstName) {
-			ToastAndroid.show('First name must be only letters.', ToastAndroid.LONG);
-			return false;
-		} else if (!isLastName) {
-			ToastAndroid.show('Last name must be only letters.', ToastAndroid.LONG);
-			return false;
-		} else if (!isPassword) {
-			ToastAndroid.show('Password must be six characters long.', ToastAndroid.LONG);
-			return false;
-		} else {
-			ToastAndroid.show('Make sure to fix your details.', ToastAndroid.LONG);
-		}
+		setIsFirstName(validator.isAlpha(firstName));
+		setIsLastName(validator.isAlpha(lastName));
+		setIsEmail(validator.isEmail(email));
+		setIsDOB(validator.isDate(DOB));
+		setIsPhone(validator.isMobilePhone(phone));
+		setIsPassword(password.length >= 6);
 	}
 
 	//Show activity spinner
@@ -140,211 +126,220 @@ export default function Register() {
 				<Text style={styles.text_header}>User Registeration</Text>
 			</View>
 
-			<Animatable.View animation='slideInUp' style={styles.footer}>
-				<Text style={styles.text_footer}>First Name</Text>
-				<View style={styles.action}>
-					<FontAwesome name='user-o' color='#05375a' size={20} />
-					<TextInput
-						placeholder='First Name'
-						style={styles.textInput}
-						autoCapitalize='none'
-						onChangeText={text => setFirstName(text)}
-					/>
-
-					{isFirstName ? null : (
-						<Animatable.View animation='fadeInLeft' duration={500}>
-							<Text style={styles.errorText}>
-								Your first name must be 4 characters long!
-							</Text>
-						</Animatable.View>
-					)}
-				</View>
-
-				<Text style={styles.text_footer}>Last Name</Text>
-				<View style={styles.action}>
-					<FontAwesome name='user' color='#05375a' size={24} />
-					<TextInput
-						placeholder='Last Name'
-						style={styles.textInput}
-						autoCapitalize='none'
-						onChangeText={text => setLastName(text)}
-					/>
-					{isLastName ? null : (
-						<Animatable.View animation='fadeInLeft' duration={500}>
-							<Text style={styles.errorText}>
-								Your last name must be 4 characters long!
-							</Text>
-						</Animatable.View>
-					)}
-				</View>
-
-				<Text style={styles.text_footer}>Email</Text>
-				<View style={styles.action}>
-					<FontAwesome name='envelope' color='#05375a' size={20} />
-					<TextInput
-						placeholder='example@email.com'
-						style={styles.textInput}
-						autoCapitalize='none'
-						onChangeText={text => setEmail(text)}
-					/>
-					{isEmail ? null : (
-						<Animatable.View animation='fadeInLeft' duration={500}>
-							<Text style={styles.errorText}>Please input your email!</Text>
-						</Animatable.View>
-					)}
-				</View>
-
-				<Text style={styles.text_footer}>Date of Birth</Text>
-				<View style={styles.action}>
-					<FontAwesome name='calendar' color='#05375a' size={20} />
-					<TextInput
-						placeholder='Enter Date of Birth'
-						value={DOB.toString()}
-						style={styles.textInput}
-						onFocus={() => {
-							Keyboard.dismiss();
-							setHideDOBPicker(prevHideDOBPicker => !prevHideDOBPicker);
-						}}
-					/>
-					<DateTimePicker
-						isVisible={!hideDOBPicker}
-						mode='date'
-						color='crimson'
-						onConfirm={date => {
-							setDOB(convertDOBToString(date));
-							setHideDOBPicker(true);
-						}}
-						onCancel={() => setHideDOBPicker(true)}
-					/>
-				</View>
-
-				<Text style={styles.text_footer}>Phone Number</Text>
-				<View style={styles.action}>
-					<FontAwesome name='phone' color='#05375a' size={20} />
-					<TextInput
-						placeholder='xxx xxx xxxx'
-						style={styles.textInput}
-						maxLength={12}
-						keyboardType='numeric'
-						onChangeText={text => setPhone(text)}
-					/>
-					{isPhone ? null : (
-						<Animatable.View animation='fadeInLeft' duration={500}>
-							<Text style={styles.errorText}>
-								Please input your phone number!
-							</Text>
-						</Animatable.View>
-					)}
-				</View>
-
-				<Text style={[styles.text_footer, {marginTop: 35}]}>Password</Text>
-				<View style={styles.action}>
-					<Feather name='key' color='#05375a' size={20} />
-					<TextInput
-						placeholder='Enter Password'
-						secureTextEntry={hidePassword}
-						style={styles.textInput}
-						autoCapitalize='none'
-						onChangeText={text => setPassword(text)}
-					/>
-
-					{isPassword ? null : (
-						<Animatable.View animation='fadeInLeft' duration={500}>
-							<Text style={styles.errorText}>
-								Your password must be 8 or more characters long!
-							</Text>
-						</Animatable.View>
-					)}
-
-					<TouchableOpacity
-						onPress={() => setHidePassword(prevHidePassword => !prevHidePassword)}>
-						{hidePassword ? (
-							<Feather name='eye-off' color='salmon' size={20} />
-						) : (
-							<Feather name='eye' color='red' size={20} />
+			<KeyboardAvoidingView behavior={'padding'} style={styles.footer}>
+				<Animatable.View animation='slideInUp'>
+					<Text style={styles.text_footer}>
+						First Name
+						{isFirstName ? null : (
+							<Animatable.View animation='fadeInLeft' duration={500}>
+								<Text style={styles.errorText}>
+									Your first name must be 4 characters long
+								</Text>
+							</Animatable.View>
 						)}
-					</TouchableOpacity>
-				</View>
+					</Text>
+					<KeyboardAvoidingView behavior={'height'} style={styles.action}>
+						<FontAwesome name='user-o' color='#05375a' size={20} />
+						<TextInput
+							placeholder='First Name'
+							style={styles.textInput}
+							autoCapitalize='none'
+							onChangeText={text => setFirstName(text)}
+						/>
+					</KeyboardAvoidingView>
 
-				<Text style={[styles.text_footer, {marginTop: 35}]}>
-					Confirm Password
-					{hidePassword ? null : (
-						<Animatable.View animation='fadeInLeft' duration={500}>
-							<Text style={styles.errorText}>passwords do not match</Text>
-						</Animatable.View>
-					)}
-				</Text>
-				<View style={styles.action}>
-					<Feather name='lock' color='#05375a' size={20} />
-					<TextInput
-						placeholder='Confirm your Password'
-						secureTextEntry={hidePassword}
-						style={styles.textInput}
-						autoCapitalize='none'
-						onChangeText={text => setPassword(text)}
-					/>
-				</View>
+					<Text style={styles.text_footer}>
+						Last Name
+						{isLastName ? null : (
+							<Animatable.View animation='fadeInLeft' duration={500}>
+								<Text style={styles.errorText}>
+									Your last name must be 4 characters long
+								</Text>
+							</Animatable.View>
+						)}
+					</Text>
+					<KeyboardAvoidingView behavior={'height'} style={styles.action}>
+						<FontAwesome name='user' color='#05375a' size={24} />
+						<TextInput
+							placeholder='Last Name'
+							style={styles.textInput}
+							autoCapitalize='none'
+							onChangeText={text => setLastName(text)}
+						/>
+					</KeyboardAvoidingView>
 
-				<View style={styles.button}>
-					<TouchableOpacity
-						onPress={() => {
-							if (validateInput()) {
-								try {
-									signUpNewUser();
-									ToastAndroid.show(
-										'Registered!',
-										ToastAndroid.SHORT
-									);
-								} catch (error) {
-									switch (error.code) {
-										case 'auth/email-already-in-use':
-											ToastAndroid.show(
-												`Email address ${email} already in use.`,
-												ToastAndroid.SHORT
-											);
-											break;
-										case 'auth/invalid-email':
-											ToastAndroid.show(
-												`Email address ${email} is invalid.`,
-												ToastAndroid.SHORT
-											);
-											break;
-										case 'auth/operation-not-allowed':
-											ToastAndroid.show(
-												`Error during sign up.`,
-												ToastAndroid.SHORT
-											);
-											break;
-										case 'auth/weak-password':
-											ToastAndroid.show(
-												'Password is not strong enough. Add additional characters including special characters and numbers.',
-												ToastAndroid.SHORT
-											);
-											break;
-										default:
-											ToastAndroid.show(
-												'Please try registration at another time',
-												ToastAndroid.SHORT
-											);
-											console.log(error.message);
-											break;
+					<Text style={styles.text_footer}>
+						Email
+						{isEmail ? null : (
+							<Animatable.View animation='fadeInLeft' duration={500}>
+								<Text style={styles.errorText}>
+									Please enter your email
+								</Text>
+							</Animatable.View>
+						)}
+					</Text>
+					<KeyboardAvoidingView behavior={'height'} style={styles.action}>
+						<FontAwesome name='envelope' color='#05375a' size={20} />
+						<TextInput
+							placeholder='example@email.com'
+							style={styles.textInput}
+							autoCapitalize='none'
+							onChangeText={text => setEmail(text)}
+						/>
+					</KeyboardAvoidingView>
+
+					<Text style={styles.text_footer}>Date of Birth{isDOB ? null : (
+							<Animatable.View animation='fadeInRight' duration={500}>
+								<Text style={styles.errorText}>
+									Please enter your date of birth
+								</Text>
+							</Animatable.View>
+						)}</Text>
+					<KeyboardAvoidingView behavior={'height'} style={styles.action}>
+						<FontAwesome name='calendar' color='#05375a' size={20} />
+						<TextInput
+							placeholder='Enter Date of Birth'
+							value={DOB.toString()}
+							style={styles.textInput}
+							onFocus={() => {
+								Keyboard.dismiss();
+								setHideDOBPicker(
+									prevHideDOBPicker => !prevHideDOBPicker
+								);
+							}}
+						/>
+						<DateTimePicker
+							isVisible={!hideDOBPicker}
+							mode='date'
+							color='crimson'
+							onConfirm={date => {
+								setDOB(convertDOBToString(date));
+								setHideDOBPicker(true);
+							}}
+							onCancel={() => setHideDOBPicker(true)}
+						/>
+					</KeyboardAvoidingView>
+
+					<Text style={styles.text_footer}>
+						Phone Number
+						{isPhone ? null : (
+							<Animatable.View animation='fadeInLeft' duration={500}>
+								<Text style={styles.errorText}>
+									Please enter your phone number
+								</Text>
+							</Animatable.View>
+						)}
+					</Text>
+					<KeyboardAvoidingView behavior={'height'} style={styles.action}>
+						<FontAwesome name='phone' color='#05375a' size={20} />
+						<TextInput
+							placeholder='xxx xxx xxxx'
+							style={styles.textInput}
+							maxLength={12}
+							keyboardType='numeric'
+							onChangeText={text => setPhone(text)}
+						/>
+					</KeyboardAvoidingView>
+
+					<Text style={styles.text_footer}>
+						Password
+						{isPassword ? null : (
+							<Animatable.View animation='fadeInRight' duration={500}>
+								<Text style={styles.errorText}>
+									Must be 6 characters or longer
+								</Text>
+							</Animatable.View>
+						)}
+					</Text>
+					<KeyboardAvoidingView behavior={'height'} style={styles.action}>
+						<Feather name='key' color='#05375a' size={20} />
+						<TextInput
+							placeholder='Enter Password'
+							secureTextEntry={hideTempPassword}
+							style={styles.textInput}
+							autoCapitalize='none'
+							onChangeText={text => setTempPassword(text)}
+						/>
+
+						<TouchableOpacity
+							onPress={() =>
+								setHideTempPassword(
+									prevHideTempPassword => !prevHideTempPassword
+								)
+							}>
+							{hideTempPassword ? (
+								<Feather name='eye-off' color='salmon' size={20} />
+							) : (
+								<Feather name='eye' color='red' size={20} />
+							)}
+						</TouchableOpacity>
+					</KeyboardAvoidingView>
+
+					<View style={styles.button}>
+						<TouchableOpacity
+							onPress={() => {
+								if (validateInput()) {
+									try {
+										signUpNewUser();
+										ToastAndroid.show(
+											'Registered!',
+											ToastAndroid.SHORT
+										);
+									} catch (error) {
+										switch (error.code) {
+											case 'auth/email-already-in-use':
+												ToastAndroid.show(
+													`Email address ${email} already in use.`,
+													ToastAndroid.SHORT
+												);
+												break;
+											case 'auth/invalid-email':
+												ToastAndroid.show(
+													`Email address ${email} is invalid.`,
+													ToastAndroid.SHORT
+												);
+												break;
+											case 'auth/operation-not-allowed':
+												ToastAndroid.show(
+													`Error during sign up.`,
+													ToastAndroid.SHORT
+												);
+												break;
+											case 'auth/weak-password':
+												ToastAndroid.show(
+													'Password is not strong enough. Add additional characters including special characters and numbers.',
+													ToastAndroid.SHORT
+												);
+												break;
+											default:
+												ToastAndroid.show(
+													'Please try registration at another time',
+													ToastAndroid.SHORT
+												);
+												console.log(
+													error.message
+												);
+												break;
+										}
 									}
 								}
-							}
-						}}
-						style={[
-							styles.signIn,
-							{
-								bordercolor: '#009387',
-								borderWidth: 1,
-								backgroundColor: 'crimson',
-								marginTop: 20,
-							},
-						]}>
-						<Text style={[styles.textSign, {color: 'white'}]}>Register</Text>
-					</TouchableOpacity>
-				</View>
-			</Animatable.View>
+							}}
+							style={[
+								styles.signIn,
+								{
+									bordercolor: '#009387',
+									borderWidth: 1,
+									backgroundColor: 'crimson',
+									marginTop: 20,
+								},
+							]}>
+							<Text style={[styles.textSign, {color: 'white'}]}>
+								Register
+							</Text>
+						</TouchableOpacity>
+					</View>
+				</Animatable.View>
+			</KeyboardAvoidingView>
 		</View>
 	);
 }
@@ -379,6 +374,7 @@ const styles = StyleSheet.create({
 		color: '#05375a',
 		fontSize: 17,
 		fontWeight: 'bold',
+		marginTop: 7,
 	},
 	action: {
 		flexDirection: 'row',
@@ -426,5 +422,6 @@ const styles = StyleSheet.create({
 	errorText: {
 		color: 'red',
 		marginLeft: 5,
+		textAlignVertical: 'bottom',
 	},
 });
